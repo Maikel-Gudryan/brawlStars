6a1da9b7c91b24f8474fe04b4123f77145e5fd52
vitorw-289 <1-25-13349@aluno.faminas.edu.br>
2026-04-08 17:39:25 -0300

    criando filtros
    eu criei filtros e o botão que filtra

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { BrawlersData } from "./componentes/Data";
 import Card from "./componentes/Card";
 import "./App.css";
- 
+
 function App() {
+  const [classeSelecionada, setClasseSelecionada] = useState ("Todos");
+
+  const classe = ["Todos", ...new Set(BrawlersData.map((b) => b.classe))];
+
+  const brawlersFiltrar = classeSelecionada === "Todos" ? BrawlersData : BrawlersData.filter((b) => b.classe === classeSelecionada);
+ 
+
   return (
     <div className="app">
       <header className="header">
@@ -10,9 +18,21 @@ function App() {
         <p className="subtitulo">Escolha seu Brawler e entre na batalha!</p>
         <span className="contador">{BrawlersData.length} Brawlers disponíveis</span>
       </header>
- 
+
+      <div className="filtro">
+          {classe.map((classe) => (
+          <button
+            key={"classe"}
+            className={`filtro-btn ${classeSelecionada === classe ? "ativo" : ""}`}
+            onClick={() => setClasseSelecionada(classe)}
+          > 
+            {classe}
+          </button>
+        ))}
+      </div>
+
         <main className="lista">
-          {BrawlersData.map((brawler) => (
+          {brawlersFiltrar.map((brawler) => (
           <Card
             key={brawler.id}
             nome={brawler.nome}
@@ -22,6 +42,14 @@ function App() {
             imagem={brawler.imagem}
           />
         ))}
+        {brawlersFiltrar.length === 0 && (
+        <p className="Sem-Resultado">Nenhum Brawler da Classe Encontrado</p>
+        )}
+
+       
+
+
+    
       </main>
     </div>
   );
